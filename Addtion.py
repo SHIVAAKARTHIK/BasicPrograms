f9b3939add4efd076d663f9f60ff5207bb095fa6
karthik.manju <karthik.manju@roi.actcorp.in>
2019-04-12 16:32:45 +0530

    1. Four Basic Programs

---
+++ Addtion.py
@@ -0,0 +1,7 @@
+#Addtion
+NUM1 = input("Enter the first NO :")
+NUM2 = input("Enter the second NO :")
+
+sum = float(NUM1) + float(NUM2)
+print(type(sum))
+print('The Sum of {} and {} is {}'.format(NUM1, NUM2, sum))
---
+++ SquareRoot.py
@@ -0,0 +1,6 @@
+NUM1 = float(input("Enter the No: "))
+
+num_sqrt = float(NUM1) ** 0.5
+
+print("The square root of %0.2f is %0.2f" % (NUM1, num_sqrt))
+
